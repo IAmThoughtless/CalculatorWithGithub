da4c1075731bdb3aa12507263a8d945f76fce42a
Orestis <orestis.tottas@gmail.com>
2025-11-02 21:43:29 +0200

    Initial Commit for this project

--- src/main.java
+++ src/main.java
@@ -4,7 +4,7 @@ public class main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
 
