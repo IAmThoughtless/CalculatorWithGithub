c584d89e5dafaa981de34020bad8f120b79d005e
Orestis <orestis.tottas@gmail.com>
2025-11-13 12:13:36 +0200

    Changes

--- calculator/src/calculator.java
+++ calculator/src/calculator.java
@@ -105,6 +105,23 @@ public class calculator implements ActionListener {
 
 
         calculator calc = new calculator();
+        if (e.getSource() == clrButton) {
+            textfield.setText("");
+        }
+
+        if (e.getSource() == delButton) {
+            String string = textfield.getText();
+            textfield.setText("");
+            for(int i=0;i<string.length()-1;i++) {
+                textfield.setText(textfield.getText()+string.charAt(i));
+            }
+        }
+
+        if (e.getSource() == negButton) {
+            double temp = Double.parseDouble(textfield.getText());
+            temp*=-1;
+            textfield.setText(String.valueOf(temp));
+        }
     }
 
 
